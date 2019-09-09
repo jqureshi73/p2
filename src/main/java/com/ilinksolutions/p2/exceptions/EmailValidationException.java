17fbde3d924d4da92b269f97ad06095b1093235f
Junaid qURESHI <jaq1973@gmail.com>
2019-09-09 10:56:29 -0400

    Fixed string issue

--- src/main/java/com/ilinksolutions/p2/exceptions/EmailValidationException.java
+++ src/main/java/com/ilinksolutions/p2/exceptions/EmailValidationException.java
@@ -4,7 +4,7 @@ public class EmailValidationException extends RuntimeException{
 	private static final long serialVersionUID = 1L;
 	
 	public EmailValidationException(String email) {
-		super(email + "is not a valid Email.");
+		super(email + " is not a valid Email.");
 	}
 
 }
