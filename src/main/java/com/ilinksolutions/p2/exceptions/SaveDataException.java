00baa30ceef009f9087020fa7b5d4e26fdaffecd
Junaid qURESHI <jaq1973@gmail.com>
2019-09-09 11:44:22 -0400

    Fixed the message

--- src/main/java/com/ilinksolutions/p2/exceptions/SaveDataException.java
+++ src/main/java/com/ilinksolutions/p2/exceptions/SaveDataException.java
@@ -4,7 +4,7 @@ public class SaveDataException extends RuntimeException{
 	private static final long serialVersionUID = 1L;
 	
     public SaveDataException(String name) {
-    	super("Failed to save the data for name: " + name);
+    	super("Failed to save the data for: " + name);
     }
 
 }
