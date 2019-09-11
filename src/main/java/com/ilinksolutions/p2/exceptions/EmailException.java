1ba416c73beaaaf1ef6abc052e0ec0fcb5fa7f19
Junaid Qureshi <jaq1973@gmail.com>
2019-09-11 17:34:47 -0400

    test

--- src/main/java/com/ilinksolutions/p2/exceptions/EmailException.java
+++ src/main/java/com/ilinksolutions/p2/exceptions/EmailException.java
@@ -1,5 +1,9 @@
 package com.ilinksolutions.p2.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.EXPECTATION_FAILED)
 public class EmailException extends RuntimeException{
 	private static final long serialVersionUID = 1L;
 	
--- src/main/java/com/ilinksolutions/p2/exceptions/UKVisaExceptionController.java
+++ src/main/java/com/ilinksolutions/p2/exceptions/UKVisaExceptionController.java
@@ -32,7 +32,7 @@ public class UKVisaExceptionController extends ResponseEntityExceptionHandler{
 	
 	@ExceptionHandler(value = EmailException.class)
 	public void emailException(HttpServletResponse response) throws IOException{
-		response.sendError(ErrorCode.EMAIL_ERROR_CODE.getLevelCode() );
+		response.sendError(HttpStatus.EXPECTATION_FAILED.value());
 	}
 	
 	
